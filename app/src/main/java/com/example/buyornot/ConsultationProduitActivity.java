a3ed00bde8a965f1e30f9a8c440cefb891a82bed
aurianeA <auriane.assemat@gmail.com>
2019-03-29 14:38:21 +0100

    Correction sur les interfaces + creation de l'activite

---
+++ app/src/main/java/com/example/buyornot/ConsultationProduitActivity.java
@@ -0,0 +1,13 @@
+package com.example.buyornot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultationProduitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_consultation_produit);
+    }
+}
