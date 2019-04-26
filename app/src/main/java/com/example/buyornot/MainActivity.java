0e2b0aaab568d2ade4c6d17a2bb8a0b96a256178
Qlecap <lecapitaine.quentin@gmail.com>
2019-04-26 08:51:31 +0200

    ajout/suppression/modification(NT)/lister

--- app/src/main/java/com/example/buyornot/MainActivity.java
+++ app/src/main/java/com/example/buyornot/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,24 +14,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Produit unProduit = new Produit(1, "Huile",
-                3336590083787L, 100, "olive", 10, 10,
-                10, 10, 10, 10, 10, 10, 11);
+        Produit unProduit = new Produit(1, "Huile", 3336590083787L, 100, "olive", 10, 10, 10, 10, 10, 10, 10, 10, 11,10);
 
-        Produit unProduit2 = new Produit(1, "Pate",
-                3336590113787L, 100, "blé", 10, 10,
-                10, 10, 10, 10, 10, 10, 11);
 
-        ProduitManager produitManager = new ProduitManager(this);
-        produitManager.open();
-        produitManager.addProduit(unProduit);
-        produitManager.addProduit(unProduit2);
+        Produit unProduit2 = new Produit(2, "Pâte", 3336592223787L, 100, "blé", 10, 10, 10, 10, 10, 10, 10, 10, 11,10);
 
-        Produit test =  produitManager.getProduit(1);
+
+        ProduitManager produitmanager = new ProduitManager(this);
+        produitmanager.open();
+        produitmanager.addProduit(unProduit);
+        produitmanager.addProduit(unProduit2);
+
+        //Produit test =  produitManager.getProduit(1);
 
         //Toast.makeText(getApplicationContext(), test.getLibelle(), Toast.LENGTH_LONG).show();
 
-        produitManager.close();
+        produitmanager.close();
     }
 
     public void ChangeActivity(View view){
